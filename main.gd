21b173fc9c416309bef4bfbdae0a08623218f447
BoppleOpple <boppleopple@gmail.com>
2026-07-23 23:35:49 -0400

    feat: implement grenade spawning

--- main.gd
+++ main.gd
@@ -1,5 +1,6 @@
 extends Node
 
+const GRENADE_SCENE = preload("res://actors/objects/grenade.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,3 +10,10 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_player_throw_grenade(position: Vector2, velocity: Vector2) -> void:
+	var newGrenade: RigidBody2D = GRENADE_SCENE.instantiate()
+	newGrenade.global_position = position
+	newGrenade.linear_velocity = velocity
+	$Objects.add_child(newGrenade)
