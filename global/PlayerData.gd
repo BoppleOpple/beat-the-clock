ce5570708fd0efed6e2e46670e51c5c36f733953
BoppleOpple <boppleopple@gmail.com>
2026-07-23 23:05:25 -0400

    refactor: alter abilities to use an enum

--- global/PlayerData.gd
+++ global/PlayerData.gd
@@ -9,12 +9,19 @@ class_name PlayerData
 # 3 - Grenade
 # -----------
 
+enum Ability {
+	EMPTY = 0,
+	DASH = 1,
+	SWORD = 2,
+	GRENADE = 3,
+}
+
 @export var player_name: String = "Player"
 @export var timer: float = 0.0
-@export var ability_1: int = 1
-@export var ability_2: int = 1
-@export var ability_3: int = 3
-@export var ability_c: int = 1
+@export var ability_1: Ability = Ability.DASH
+@export var ability_2: Ability = Ability.DASH
+@export var ability_3: Ability = Ability.GRENADE
+@export var ability_c: Ability = Ability.DASH
 @export var ability_1_cooldown: float = 0.0
 @export var ability_2_cooldown: float = 0.0
 @export var ability_3_cooldown: float = 0.0
