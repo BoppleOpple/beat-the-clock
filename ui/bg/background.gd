3f17d8c9ab13107e0f4d80821505c2a01a3fa71d
AlaskanGopher <33465052+AlaskanGopher@users.noreply.github.com>
2026-07-25 08:44:57 -0800

    Merging to match repos

--- ui/bg/background.gd
+++ ui/bg/background.gd
@@ -10,13 +10,14 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	$BG.scroll_offset -= Vector2(20,0) * delta
 
-func _bg_select() -> void:
-	var random = randi_range(1,8)
-	$BG/"1"/Texture.texture = load("res://assets/textures/Clouds/Clouds " + str(random) + "/1.png")
-	$BG/"2"/Texture.texture = load("res://assets/textures/Clouds/Clouds " + str(random) + "/2.png")
-	$BG/"3"/Texture.texture = load("res://assets/textures/Clouds/Clouds " + str(random) + "/3.png")
-	$BG/"4"/Texture.texture = load("res://assets/textures/Clouds/Clouds " + str(random) + "/4.png")
-	if ResourceLoader.exists("res://assets/textures/Clouds/Clouds " + str(random) + "/5.png"):
-		$BG/"5"/Texture.texture = load("res://assets/textures/Clouds/Clouds " + str(random) + "/5.png")
-	if ResourceLoader.exists("res://assets/textures/Clouds/Clouds " + str(random) + "/6.png"):
-		$BG/"6"/Texture.texture = load("res://assets/textures/Clouds/Clouds " + str(random) + "/6.png")
+func _bg_select(choice: int = 0) -> void:
+	if choice == 0:
+		choice = randi_range(1,8)
+	$BG/"1"/Texture.texture = load("res://assets/textures/Clouds/Clouds " + str(choice) + "/1.png")
+	$BG/"2"/Texture.texture = load("res://assets/textures/Clouds/Clouds " + str(choice) + "/2.png")
+	$BG/"3"/Texture.texture = load("res://assets/textures/Clouds/Clouds " + str(choice) + "/3.png")
+	$BG/"4"/Texture.texture = load("res://assets/textures/Clouds/Clouds " + str(choice) + "/4.png")
+	if ResourceLoader.exists("res://assets/textures/Clouds/Clouds " + str(choice) + "/5.png"):
+		$BG/"5"/Texture.texture = load("res://assets/textures/Clouds/Clouds " + str(choice) + "/5.png")
+	if ResourceLoader.exists("res://assets/textures/Clouds/Clouds " + str(choice) + "/6.png"):
+		$BG/"6"/Texture.texture = load("res://assets/textures/Clouds/Clouds " + str(choice) + "/6.png")
